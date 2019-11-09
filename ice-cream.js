19eb34fd04d00f097e6669b7373ae92196d23de9
Katherine <katherine.szelag@mail.utoronto.ca>
2019-11-09 04:17:49 +0000

    icecream array challenge

---
+++ ice-cream.js
@@ -0,0 +1,12 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+const newIceCreamFlavours = iceCreamFlavours.concat("root beer");
+console.log(newIceCreamFlavours);
+//console.log the first flavour in the array.
+
+console.log(newIceCreamFlavours[0]);
+console.log(newIceCreamFlavours[5]);
+console.log(newIceCreamFlavours.length);
